26e051559a945af4d9527601adeddeffa7be237b
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2025-05-18 14:04:58 -0400

    Updates

--- README.md
+++ README.md
@@ -11,17 +11,21 @@ docker run \
 
 ## Notes
 
+### Onimages
+
+Onimages are designed to run applications in proxy mode, which means that they don't work with how OnionShare works.
+
+OnionShare calls `create_ephemeral_hidden_service`, which sends an [`ADD_ONION`](https://spec.torproject.org/control-spec/commands.html#add_onion) command with the private key (or a new key is generated). There's no way to get the tor daemon to load in the key outside of the process (as of writing).
+
 ### Onion v3 Keys
 
-`service_id` is the onion address afaik ([spec](https://spec.torproject.org/rend-spec/encoding-onion-addresses.html))
+`service_id` is the onion address without the `".onion"` TLD ([spec](https://spec.torproject.org/rend-spec/encoding-onion-addresses.html))
 
-`private_key` appears to be 64 bytes, which means it's the "expanded" key. Onion v3 uses weird key blinding / expansion (I'm having a headache thinking about it) ([spec](https://spec.torproject.org/rend-spec/keyblinding-scheme.html)).
+You can derive the public key from the onion address (`service_id`) and vice versa.
 
-The cryptography is weird, but you can re-derive the public key from the service_id.
+`private_key` is the base64 "expanded" private key. Tor uses "expanded" private keys instead of the actual private keys. The process of "expanding" the private key isn't reversible (involves hashing and bit clamping). You can't derive the public key from the "expanded" private key.
 
-You cannot derive the public key from the "expanded" private key because of the key blinding.
-
-I have scripts that convert `hs_ed25519_secret_key` and `hs_ed25519_public_key` files into partial Onionshare config (and the reverse)
+I have scripts that convert `hs_ed25519_secret_key` and `hs_ed25519_public_key` files into partial OnionShare config (and vice versa).
 
 ### Synology DSM
 
--- scripts/onionshare_keys.py
+++ scripts/onionshare_keys.py
@@ -2,54 +2,81 @@ import json
 import base64
 import hashlib
 
-def parse_conf():
-    config = {}
-    with open('onionshare.json', 'r') as f:
-        config = json.load(f)
+
+def pubkey_to_service_id(pubkey: bytes) -> str:
+    """
+    Returns service id (onion addr) for the specified v3 onion addr (PUBKEY).
+
+    CHECKSUM = SHA3_256(".onion checksum" | PUBKEY | VERSION)[:2]
+    service_id = base32(PUBKEY | CHECKSUM | VERSION)
+    onion_address = service_id + ".onion"
+
+    See https://spec.torproject.org/rend-spec/encoding-onion-addresses.html
+    """
+    version = b"\x03"
+    checksum = hashlib.sha3_256(b".onion checksum" + pubkey + version).digest()[:2]
+    return base64.b32encode(pubkey + checksum + version).decode().lower()
+
+
+def get_keys_from_onionshare(config = {}) -> tuple[bytes, bytes]:
+    """
+    Extract onion v3 key files from onionshare config
+    """
+    service_id = config.get("general", {}).get("service_id", "")
+    public_key = bytearray(b"== ed25519v1-public: type0 ==\x00\x00\x00")
+    public_key.extend(base64.b32decode(service_id)[:-3])
 
     private_key = config.get("onion", {}).get("private_key", "").encode('ascii')
+    secret_key = bytearray(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
+    secret_key.extend(base64.b64decode(private_key))
 
-    with open('hs_ed25519_secret_key', 'wb') as f:
-        f.write(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
-        f.write(base64.b64decode(private_key))
+    return (bytes(public_key), bytes(secret_key))
 
-    service_id = config.get("general", {}).get("service_id", "")
 
-    with open('hs_ed25519_public_key', 'wb') as f:
-        f.write(b"== ed25519v1-public: type0 ==\x00\x00\x00")
-        f.write(base64.b32decode(service_id)[:-3])
+def add_keys_to_onionshare(config = {}, public_key = b'', secret_key = b'') -> dict:
+    """
+    Add onion v3 key files to onionshare config
+    """
 
-def gen_conf():
+    if public_key.startswith(b"== ed25519v1-public: type0 ==\x00\x00\x00"):
+        service_id = pubkey_to_service_id(
+            public_key.removeprefix(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
+        )
+        config.setdefault("general", {})
+        config["general"]["service_id"] = service_id
+
+    if secret_key.startswith(b"== ed25519v1-secret: type0 ==\x00\x00\x00"):
+        private_key = base64.b64encode(
+            secret_key.removeprefix(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
+        ).decode('ascii')
+        config.setdefault("onion", {})
+        config["onion"]["private_key"] = private_key
+
+    return config
+
+
+def main():
     config = {}
 
-    secret_key = b''
-    with open('hs_ed25519_secret_key', 'rb') as f:
-        secret_key = f.read().strip()
+    # Get Keys from Config
+    with open('onionshare.json', 'r') as f:
+        config = json.load(f)
+    public_key, secret_key = get_keys_from_onionshare(config)
+    with open('hs_ed25519_secret_key', 'wb') as f:
+        f.write(secret_key)
+    with open('hs_ed25519_public_key', 'wb') as f:
+        f.write(public_key)
 
-    assert secret_key.startswith(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
-
-    private_key = base64.b64encode(
-        secret_key.removeprefix(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
-    ).decode('ascii')
-
-    config["onion"] = {"private_key": private_key}
-
+    # Add Keys to Config
     public_key = b''
     with open('hs_ed25519_public_key', 'rb') as f:
         public_key = f.read().strip()
-
-    assert public_key.startswith(b"== ed25519v1-public: type0 ==\x00\x00\x00")
-
-    def _service_id_from_public_key(public_key: bytes) -> str:
-        version = b"\x03"
-        checksum = hashlib.sha3_256(b".onion checksum" + public_key + version).digest()[:2]
-        return base64.b32encode(public_key + checksum + version).decode().lower()
-
-    service_id = _service_id_from_public_key(
-        public_key.removeprefix(b"== ed25519v1-secret: type0 ==\x00\x00\x00")
-    )
-
-    config["general"] = {"service_id": service_id}
-
+    secret_key = b''
+    with open('hs_ed25519_secret_key', 'rb') as f:
+        secret_key = f.read().strip()
+    config = add_keys_to_onionshare(config, public_key, secret_key)
     with open('onionshare.json', 'w') as f:
-        json.dump(f)
+        json.dump(config, f, indent=2)
+
+if __name__ == "__main__":
+    main()
